1e9e24e5e2a57b434d356d53bc2e229e5485296b
cclauss <cclauss@bluewin.ch>
2016-03-04 14:49:20 +0100

    passages_as_dicts() converts multiple refs into a list of dicts
    passages_as_dicts() converts multiple refs into a list of dicts

--- BiblePassageAsDict.py
+++ BiblePassageAsDict.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import json, requests
 
-def passage_as_dict(ref, version):
+def passage_as_dict(ref, version='nasb'):
     '''getbible.net does not valid json so we convert (content); to [content]'''
     fmt = 'https://getbible.net/json?p={}&v={}'
     url = fmt.format(ref.replace(' ', '%20'), version)
     return json.loads('[{}]'.format(requests.get(url).text[1:-2]))
 
-print(passage_as_dict('Luke 3:1', 'nasb'))
+def passages_as_dicts(ref, version='nasb'):
+    return [passage_as_dict(p.strip(), version)
+            for p in ref.split(';') if p.strip()]
+
+# Matthew is 'type': 'book', Mark is 'type': 'chapter', Luke and John are 'type': 'verse'
+passages = passages_as_dicts('Matthew;Mark 1;Luke 1:1;John 1:1-3')
+print(json.dumps(passages, indent=4, sort_keys=True))
